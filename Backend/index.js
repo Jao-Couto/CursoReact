02144765478cada4beaa607e92b2a60a4c16a890
João Couto <73503848+Jao-Couto@users.noreply.github.com>
2022-10-09 16:54:11 -0300

    api init

--- Backend/index.js
+++ Backend/index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose')
+const Tasks = require('./modules/Tasks')
 
 // crio um servidor express
 const app = express();
@@ -13,5 +15,40 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
+app.post('/add', async (req, res) => {
+    const { titulo, descricao, data, executada } = res.req.body
+    const task = {
+        titulo,
+        descricao,
+        data,
+        executada
+    }
+    await Tasks.create(task)
+        .then(() => {
+            res.status(201).json({ status: 'ok' })
+        }).catch((err) => {
+            res.status(500).json({ error: err })
+        })
+})
+
+app.get('/get', async (req, res) => {
+    await Tasks.find()
+        .then((result) => {
+            res.status(201).json({ result })
+        }).catch((err) => {
+            res.status(500).json({ error: err })
+        })
+    res.json({ nome: 'ola' })
+})
+
+mongoose.connect('mongodb+srv://admin:adminJoaoRafael@expresssecomp2022.o7wdbuy.mongodb.net/?retryWrites=true&w=majority')
+    .then(() => {
+        console.log('conectado ao mongoose');
+        app.listen(9000, () => console.log('Express started at http://localhost:9000'));
+    }).catch((err) => {
+        console.log('erro ao conectar no mongoose: ', err);
+    })
+
 // o servidor irá rodar dentro da porta 9000
-app.listen(9000, () => console.log('Express started at http://localhost:9000'));
+
+
