3baaa65b19b830d15269812aaa73a8dd1fd212fb
João Couto <73503848+Jao-Couto@users.noreply.github.com>
2022-10-09 16:27:38 -0300

    att

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Header from "./components/Header";
 import Tasks from "./components/Tasks";
 import Modal from './components/Modal';
@@ -6,20 +6,19 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
     const [tasks, setTasks] = useState([])
+    const [tasksExecutadas, setTasksExecutadas] = useState([])
 
     const [dropdown, setDropdown] = useState("");
-
-
 
     return (
         <Router>
             <Header setDropdown={(value) => setDropdown(value)}></Header>
             <Modal className={dropdown} setTasks={(task) => setTasks([...tasks, task])} setDropdown={(value) => setDropdown(value)} />
             <Routes>
-                <Route path="/hoje" element={<Tasks hoje setTasks={(task) => setTasks(task)} tasks={tasks}></Tasks>}></Route>
-                <Route path="/atrasadas" element={<Tasks atrasadas setTasks={(task) => setTasks(task)} tasks={tasks}></Tasks>}></Route>
-                <Route path="/proximas" element={<Tasks proximas setTasks={(task) => setTasks(task)} tasks={tasks}></Tasks>}></Route>
-                <Route path="/executadas" element={<Tasks executadas setTasks={(task) => setTasks(task)} tasks={tasks}></Tasks>}></Route>
+                <Route path="/hoje" element={<Tasks hoje setTasks={(task) => setTasks(task)} tasks={tasks} setTasksExecutadas={(task) => setTasksExecutadas(task)} tasksExecutadas={tasksExecutadas}></Tasks>}></Route>
+                <Route path="/atrasadas" element={<Tasks atrasadas setTasks={(task) => setTasks(task)} tasks={tasks} setTasksExecutadas={(task) => setTasksExecutadas(task)} tasksExecutadas={tasksExecutadas}></Tasks>}></Route>
+                <Route path="/proximas" element={<Tasks proximas setTasks={(task) => setTasks(task)} tasks={tasks} setTasksExecutadas={(task) => setTasksExecutadas(task)} tasksExecutadas={tasksExecutadas}></Tasks>}></Route>
+                <Route path="/executadas" element={<Tasks executadas setTasks={(task) => setTasks(task)} tasks={tasks} setTasksExecutadas={(task) => setTasksExecutadas(task)} tasksExecutadas={tasksExecutadas}></Tasks>}></Route>
             </Routes>
         </Router>
     );
--- todo/src/components/Tasks/index.js
+++ todo/src/components/Tasks/index.js
@@ -1,51 +1,65 @@
+import { useEffect } from "react"
 import "./Tasks.css"
 
-function Tasks({ tasks, setTasks, hoje = false, atrasadas = false, proximas = false, executadas = false }) {
+function Tasks({ tasks, setTasks, tasksExecutadas, setTasksExecutadas, hoje = false, atrasadas = false, proximas = false, executadas = false }) {
+
 
     const excluirTask = (index) => {
         const tasksExcluida = tasks.pop(index)
         setTasks(tasksExcluida)
     }
 
+
     const executarTask = (index) => {
-        let tasksExecutadas = tasks
-        tasksExecutadas[index].executada = true
-        console.log(tasksExecutadas);
-        setTasks(tasksExecutadas)
+        setTasksExecutadas([...tasksExecutadas, tasks[index]])
+        const tasksExcluida = tasks.pop(index)
+        setTasks(tasksExcluida)
+    }
+
+    const imprimirTasks = () => {
+        let tasksImpirmir
+
+        if (executadas)
+            tasksImpirmir = tasksExecutadas
+        else
+            tasksImpirmir = tasks
+
+        console.log("exec", tasksImpirmir);
+        return tasksImpirmir.length > 0 && tasksImpirmir.map((item, index) => {
+            console.log("entrou");
+            let dataComparar = new Date(item.data);
+            dataComparar.setDate(dataComparar.getDate() + 1);
+            dataComparar = dataComparar.setHours(0, 0, 0, 0)
+
+            const dataHoje = new Date().setHours(0, 0, 0, 0)
+
+            const ifHoje = dataComparar === dataHoje && hoje;
+            const ifProxima = dataComparar > dataHoje && proximas
+            const ifAtrasada = dataComparar < dataHoje && atrasadas
+
+            if ((((ifHoje) || (ifProxima) || (ifAtrasada)) && !item.executada) || executadas)
+                return <div className="tasks" key={index}>
+
+                    <h3>{item.titulo}</h3>
+                    <div>
+                        <img src="https://cdn.pixabay.com/photo/2016/02/01/12/20/play-1173495_1280.png" alt="Play" onClick={() => { executarTask(index) }}></img>
+                        <img src="https://cdn.pixabay.com/photo/2014/03/24/13/41/trashcan-293989_1280.png" alt="Lixo" onClick={() => excluirTask(index)}></img>
+                        <img src="https://cdn.pixabay.com/photo/2016/07/26/05/18/pencil-1542024_1280.png" alt="Editar" ></img>
+                    </div>
+
+                    <p>{item.descricao}</p>
+                    <h5>{item.data.split('-').reverse().join('/')}</h5>
+                </div>
+        })
     }
 
     return (
         <section style={{ display: "flex", alignItems: 'center', flexDirection: 'column' }}>
-            {tasks.length > 0 && tasks.map((item, index) => {
-                let dataComparar = new Date(item.data);
-                dataComparar.setDate(dataComparar.getDate() + 1);
-                dataComparar = dataComparar.setHours(0, 0, 0, 0)
-
-                const dataHoje = new Date().setHours(0, 0, 0, 0)
-
-                const ifHoje = dataComparar === dataHoje && hoje;
-                const ifProxima = dataComparar > dataHoje && proximas
-                const ifAtrasada = dataComparar < dataHoje && atrasadas
-                const ifExecutada = executadas && item.executada
-
-                console.log(ifHoje,
-                    ifProxima,
-                    ifAtrasada,
-                    ifExecutada)
-                if ((((ifHoje) || (ifProxima) || (ifAtrasada)) && !item.executada) || (ifExecutada))
-                    return <div className="tasks" key={index}>
-
-                        <h3>{item.titulo}</h3>
-                        <div>
-                            <img src="https://cdn.pixabay.com/photo/2016/02/01/12/20/play-1173495_1280.png" alt="Play" onClick={() => { executarTask(index) }}></img>
-                            <img src="https://cdn.pixabay.com/photo/2014/03/24/13/41/trashcan-293989_1280.png" alt="Lixo" onClick={() => excluirTask(index)}></img>
-                            <img src="https://cdn.pixabay.com/photo/2016/07/26/05/18/pencil-1542024_1280.png" alt="Editar" ></img>
-                        </div>
-
-                        <p>{item.descricao}</p>
-                        <h5>{item.data.split('-').reverse().join('/')}</h5>
-                    </div>
-            })}
+            {hoje && <h1>Hoje</h1>}
+            {atrasadas && <h1>Atrasadas</h1>}
+            {proximas && <h1>Próximas</h1>}
+            {executadas && <h1>Executadas</h1>}
+            {imprimirTasks()}
         </section>
     )
 }
